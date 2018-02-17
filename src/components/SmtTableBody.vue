e524355608b68134569550cdac54ad680794e42f
muhkuhxy <>
2018-02-17 16:24:10 +0100

    eye candy

--- src/components/SmtTableBody.vue
+++ src/components/SmtTableBody.vue
@@ -53,28 +53,38 @@ export default {
     </div>
     <div class="cell main vertical-middle serviceweek">
       <div>
-        <input v-if="notSunday" type="checkbox" :checked="isTuesday" @change.stop.prevent="change()" />
+        <toggle-button v-if="notSunday" :value="isTuesday" :sync="true" @change="change()" :height="17" :width="44"/>
       </div>
     </div>
     <div class="cell main col">
       <div class="cell sub" :class="classObject" data-service="sicherheit" data-index="0">{{ name('sicherheit', 0) }}
-        <div class="remove" v-if="selected && assignment.sicherheit[0]" @click="remove('sicherheit', 0)"><span class="badge"><span class="glyphicon glyphicon-remove"></span></span></div>
+        <transition name="bounce">
+          <div class="remove" v-if="selected && assignment.sicherheit[0]" @click="remove('sicherheit', 0)"><span class="badge"><span class="glyphicon glyphicon-remove"></span></span></div>
+        </transition>
       </div>
       <div class="cell sub" :class="classObject" data-service="sicherheit" data-index="1">{{ name('sicherheit', 1) }}
-        <div class="remove" v-if="selected && assignment.sicherheit[1]" @click="remove('sicherheit', 1)"><span class="badge"><span class="glyphicon glyphicon-remove"></span></span></div>
+        <transition name="bounce">
+          <div class="remove" v-if="selected && assignment.sicherheit[1]" @click="remove('sicherheit', 1)"><span class="badge"><span class="glyphicon glyphicon-remove"></span></span></div>
+        </transition>
       </div>
     </div>
     <div class="cell main col">
       <div class="cell sub" :class="classObject" data-service="mikro" data-index="0">{{ name('mikro', 0) }}
-        <div class="remove" v-if="selected && assignment.mikro[0]" @click="remove('mikro', 0)"><span class="badge"><span class="glyphicon glyphicon-remove"></span></span></div>
+        <transition name="bounce">
+          <div class="remove" v-if="selected && assignment.mikro[0]" @click="remove('mikro', 0)"><span class="badge"><span class="glyphicon glyphicon-remove"></span></span></div>
+        </transition>
       </div>
       <div class="cell sub" :class="classObject" data-service="mikro" data-index="1">{{ name('mikro', 1) }}
-        <div class="remove" v-if="selected && assignment.mikro[1]" @click="remove('mikro', 1)"><span class="badge"><span class="glyphicon glyphicon-remove"></span></span></div>
+        <transition name="bounce">
+          <div class="remove" v-if="selected && assignment.mikro[1]" @click="remove('mikro', 1)"><span class="badge"><span class="glyphicon glyphicon-remove"></span></span></div>
+        </transition>
       </div>
     </div>
     <div class="cell main vertical-middle" :class="classObject" data-service="tonanlage" data-index="0">
       <div>{{ name('tonanlage', 0) }}
-        <div class="remove" v-if="selected && assignment.tonanlage[0]" @click="remove('tonanlage', 0)"><span class="badge"><span class="glyphicon glyphicon-remove"></span></span></div>
+        <transition name="bounce">
+          <div class="remove" v-if="selected && assignment.tonanlage[0]" @click="remove('tonanlage', 0)"><span class="badge"><span class="glyphicon glyphicon-remove"></span></span></div>
+        </transition>
       </div>
     </div>
   </div>
@@ -116,6 +126,21 @@ export default {
   .platzhalter {
     border: 1px inset !important;
     position: relative;
+  }
+  $bounce: .075s;
+  .bounce-enter-active {
+    animation: bounce-in $bounce;
+  }
+  .bounce-leave-active {
+    animation: bounce-in $bounce reverse;
+  }
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    100% {
+      transform: scale(1);
+    }
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import moment from 'moment'
 import store from './store'
 import router from './router'
 import App from './App'
+import ToggleButton from 'vue-js-toggle-button'
 
 Vue.config.productionTip = false
 
@@ -15,6 +16,8 @@ Vue.filter('dateLong', function (value) {
   if (!value) return ''
   return moment(value).format('dddd, DD.MM.YYYY')
 })
+
+Vue.use(ToggleButton)
 
 // eslint-disable-next-line no-new
 new Vue({
