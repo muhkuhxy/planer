c749dcfe8319ca3420a4ca04b4628f0cd718e385
Timon Loeffen <timon.loeffen@gmail.com>
2019-12-18 22:03:18 +0100

    remove no-console eslint rule

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/frontend' : '/',
   outputDir: '../public/frontend',
   devServer: {
-    proxy: 'http://localhost:9000',
+    proxy: 'http://localhost:9000'
   }
 }
